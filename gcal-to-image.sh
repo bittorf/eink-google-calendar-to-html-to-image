bc3f126ef2fe846b8785e9dda9d726b6e04bddbf
Bastian Bittorf <bb@npl.de>
2024-01-06 16:47:49 +0100

    limit calendar entries to 10 lines max - also several fixes for HTML output

--- gcal-to-image.sh
+++ gcal-to-image.sh
@@ -3,6 +3,7 @@
 #
 # e.g.: ssh-copy-id root@10.63.44.33
 WEBSERVER_UPLOAD='root@10.63.44.33:/www/eink-image.png'
+MAX_LINES=10
 
 command -v 'phantomjs' >/dev/null || {
 	URL="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
@@ -29,38 +30,67 @@ query()
 
 NBS='&nbsp;'
 TEMP="$( mktemp )" || exit 1
-query | tr -d '\r' >"$TEMP"
+GCAL_PLAINTTEXT="$( mktemp )" || exit 1
+query | tr -d '\r' >"$GCAL_PLAINTTEXT"
 
 emit_html()
 {
 echo "<!DOCTYPE html><html lang=en><head><title>gcalcli-html-export</title>"
 echo "<meta charset='UTF-8'></head><body bgcolor=white>"
 echo "<table cellspacing=1 cellpadding=1 width=100% border=1 height=100%>"
-echo "<tr><th align=center width=1%>Day</th><th align=center width=1%>Time</th><th align=left width=98%>${NBS}${NBS}Mission</th></tr>"
+echo " <tr>"
+echo "  <th align=center width=1%>Day</th>"
+echo "  <th align=center width=1%>Time</th>"
+echo "  <th align=left width=98%>${NBS}${NBS}Mission</th>"
+echo " </tr>"
 
+CUTPATTERN=
+I=0
+J=0
 while IFS= read -r LINE; do {
+  I=$(( I + 1 ))
+
   case "$LINE" in
     '')
+      I=$(( I - 1 ))
     ;;
     "Fri "*|"Sat "*|"Sun "*|"Thu "*|"Tue "*|"Wed "*|"Mon "*)
+      J=$(( J + 1 ))
+      test $I -ge $MAX_LINES && test -z "$CUTPATTERN" && CUTPATTERN='please-cut-here'
+
       # shellcheck disable=SC2086
       MISSION="$( echo "$LINE" | cut -b20-999 )" && set -- $MISSION && MISSION=$*
-      TIME="$(    echo "$LINE" | cut -b13-17 )"
+      TIME="$(    echo "$LINE" | cut -b13-17 )" && set -- $TIME && TIME=$* && TIME="${TIME:-${NBS}}"
       DAY="$(     echo "$LINE" | cut -b1-10 )"
-      echo "<tr><td nowrap><tt>$DAY</tt></td><td align=right valign=top nowrap><tt>$TIME</tt></td><td>${NBS}${NBS}$MISSION<td></tr>"
+
+      echo " <tr><!-- startofentry $J | line: $I | $CUTPATTERN -->"
+      echo "  <td nowrap><tt>$DAY</tt></td>"
+      echo "  <td align=right valign=top nowrap><tt>$TIME</tt></td>"
+      echo "  <td>${NBS}${NBS}$MISSION</td>"
+      echo " </tr>"
     ;;
     *)
       # shellcheck disable=SC2086
       MISSION="$( echo "$LINE" | cut -b20-999 )" && set -- $MISSION && MISSION=$*
-      TIME="$(    echo "$LINE" | cut -b13-17 )"
+      TIME="$(    echo "$LINE" | cut -b13-17 )" && set -- $TIME && TIME=$* && TIME="${TIME:-${NBS}}"
       DAY="$NBS"
-      echo "<tr><td nowrap>$DAY</td><td align=right valign=top nowrap><tt>$TIME</tt></td><td>${NBS}${NBS}$MISSION<td></tr>"
+
+      echo " <tr><!-- line: $I -->"
+      echo "  <td nowrap>$DAY</td>"
+      echo "  <td align=right valign=top nowrap><tt>$TIME</tt></td>"
+      echo "  <td>${NBS}${NBS}$MISSION</td>"
+      echo " </tr>"
     ;;
   esac
-} done <"$TEMP"
+} done <"$GCAL_PLAINTTEXT" >"$TEMP"
 
-echo "</table><body></html>"
-rm "$TEMP"
+  if [ -n "$CUTPATTERN" ]; then
+    sed -n '1,/please-cut-here/p' "$TEMP" | head -n -1
+  else
+    cat "$TEMP"
+  fi
+
+  echo "</table></body></html>"
 }
 
 html_screenshot()
@@ -95,4 +125,5 @@ IMAGE="$( html_screenshot "file://$TEMPFILE.html" plan.png )" && \
 convert "$IMAGE" -type GrayScale -depth 8 -colors 256 -resize '800x600!' -rotate 90 "$TEMPFILE.png"
 scp -O "$TEMPFILE.png" "$WEBSERVER_UPLOAD"
 
-rm -f "$TEMPFILE" "$TEMPFILE.html" "$TEMPFILE.png" "$IMAGE"
+cp "$TEMPFILE.html" /tmp/1.html
+rm -f "$TEMPFILE" "$TEMPFILE.html" "$TEMPFILE.png" "$IMAGE" "$GCAL_PLAINTTEXT" "$TEMP"
